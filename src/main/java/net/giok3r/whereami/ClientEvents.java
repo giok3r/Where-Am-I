851b799e5516414330214e96a446d1e3bd1df087
SilentChaos512 <sworkman256@gmail.com>
2025-04-21 21:23:45 -0500

    Fixes #1, time displaying incorrectly after first day; format code

--- src/main/java/net/giok3r/whereami/ClientEvents.java
+++ src/main/java/net/giok3r/whereami/ClientEvents.java
@@ -16,17 +16,17 @@ import net.neoforged.neoforge.client.event.RenderGuiEvent;
 public class ClientEvents {
     @SubscribeEvent
     public static void onRenderGui(RenderGuiEvent.Post event) {
-        LocalPlayer player= Minecraft.getInstance().player;
-        if(player==null) {
+        LocalPlayer player = Minecraft.getInstance().player;
+        if (player == null) {
             return;
         }
         int x = (int) player.position().x;
         int y = (int) player.position().y;
         int z = (int) player.position().z;
 
-        Font font= Minecraft.getInstance().font;
-        Component text= Component.literal(x + ", " + y +", " + z);
-        event.getGuiGraphics().drawString(font, text,10,10, -1, true);
+        Font font = Minecraft.getInstance().font;
+        Component text = Component.literal(x + ", " + y + ", " + z);
+        event.getGuiGraphics().drawString(font, text, 10, 10, -1, true);
 
         Level level = player.level();
         Holder<Biome> biomeHolder = level.getBiome(player.blockPosition());
@@ -36,20 +36,20 @@ public class ClientEvents {
             event.getGuiGraphics().drawString(font, biomeName, 10, 20, 0xFFCCCCCC, true);
         }
 
-        int time = (int) level.getDayTime();
+        int time = (int) level.getDayTime() % 24_000;
         int hour;
         String amOrPm;
-        if (time <7000) {
+        if (time < 7000) {
             // 6am-12pm
-            hour = time/1000+6;
+            hour = time / 1000 + 6;
             if (hour == 12) {
                 amOrPm = "PM";
             } else {
                 amOrPm = "AM";
             }
-        } else if (time <19_000) {
+        } else if (time < 19_000) {
             // 1pm-12am
-            hour = time/1000-6;
+            hour = time / 1000 - 6;
             if (hour == 12) {
                 amOrPm = "AM";
             } else {
@@ -57,7 +57,7 @@ public class ClientEvents {
             }
         } else {
             // 1am-5am
-            hour = time/1000-18;
+            hour = time / 1000 - 18;
             amOrPm = "AM";
         }
         int minute = (int) ((time % 1000) * 0.06);
