cb0c63125766c354da031319c00595395ec04173
SilentChaos512 <sworkman256@gmail.com>
2026-07-25 12:25:02 -0500

    Update to 26.2

--- src/main/java/net/giok3r/whereami/ClientEvents.java
+++ src/main/java/net/giok3r/whereami/ClientEvents.java
@@ -18,7 +18,7 @@ public class ClientEvents {
     @SubscribeEvent
     public static void onRenderGui(RenderGuiEvent.Post event) {
         Minecraft mc = Minecraft.getInstance();
-        if (mc.getDebugOverlay().showDebugScreen() || mc.options.hideGui) {
+        if (mc.getDebugOverlay().showDebugScreen() || mc.gui.hud.isHidden()) {
             return;
         }
 
